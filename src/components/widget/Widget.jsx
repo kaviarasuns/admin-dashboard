7a70980ea48ad087f2c07cc817d7a14aeda5f8f5
KaviArasuNS <KaviArasu_NS@icloud.com>
2022-08-27 21:09:50 +0530

    widgets update

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,55 +1,75 @@
-import "./widget.scss"
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
-import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
+import "./widget.scss";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
 
-
-const Widget = ({type}) => {
+const Widget = ({ type }) => {
   let data;
 
   //temporary
   const diff = 20;
 
-  switch(type){
+  switch (type) {
     case "user":
       data = {
-        title:"EARNINGS(MONTHLY)",
+        title: "EARNINGS(MONTHLY)",
         isMoney: false,
-        icon: <PersonOutlineIcon className="icon" style={{color: "crimson", backgroundColor: "rgba(255,0,0,0.2)"}}/>,
-        amount: "$40,000"
+        icon: (
+          <PersonOutlineIcon
+            className="icon"
+            style={{ color: "crimson", backgroundColor: "rgba(255,0,0,0.2)" }}
+          />
+        ),
+        amount: "₹40,000",
       };
-        break;
-        case "order":
+      break;
+    case "order":
       data = {
-        title:"EARNINGS (ANNUAL)",
+        title: "EARNINGS (ANNUAL)",
         isMoney: false,
-        icon: <ShoppingCartOutlinedIcon className="icon" style={{color: "goldenrod", backgroundColor: "rgba(218,165,32,0.2)"}}/>,
-        amount: "$215,000"
+        icon: (
+          <ShoppingCartOutlinedIcon
+            className="icon"
+            style={{
+              color: "goldenrod",
+              backgroundColor: "rgba(218,165,32,0.2)",
+            }}
+          />
+        ),
+        amount: "₹215,000",
       };
-        break;
-        case "earning":
-        data = {
-        title:"TASKS",
+      break;
+    case "earning":
+      data = {
+        title: "TASKS",
         isMoney: true,
-        icon: <MonetizationOnOutlinedIcon className="icon" style={{color: "green", backgroundColor: "rgba(0,128,0,0.2)"}}/>,
-        amount: "50%"  
+        icon: (
+          <MonetizationOnOutlinedIcon
+            className="icon"
+            style={{ color: "green", backgroundColor: "rgba(0,128,0,0.2)" }}
+          />
+        ),
+        amount: "50%",
       };
-        break;
-        case "balance":
-        data = {
-        title:"PENDING REQUESTS",
+      break;
+    case "balance":
+      data = {
+        title: "PENDING REQUESTS",
         isMoney: true,
-        icon: <AccountBalanceWalletOutlinedIcon className="icon" style={{color: "purple", backgroundColor: "rgba(128,0,128,0.2)"}}/>,
+        icon: (
+          <AccountBalanceWalletOutlinedIcon
+            className="icon"
+            style={{ color: "purple", backgroundColor: "rgba(128,0,128,0.2)" }}
+          />
+        ),
         amount: "18",
       };
-        break;
-        default:
-          break;
-      
-    }
-  
+      break;
+    default:
+      break;
+  }
 
   return (
     <div className="widget">
@@ -60,12 +80,12 @@ const Widget = ({type}) => {
       </div>
       <div className="right">
         <div className="percentage positive">
-          <KeyboardArrowUpIcon/>
-          </div>
-          {data.icon}
+          <KeyboardArrowUpIcon />
+        </div>
+        {data.icon}
       </div>
     </div>
-  )
-  }
+  );
+};
 
-export default Widget
+export default Widget;
